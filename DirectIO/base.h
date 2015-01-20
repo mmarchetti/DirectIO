26f4e634161a8c1040b489474400f1b099519c71
Michael Marchetti <mmarchetti@rochester.rr.com>
2015-01-20 09:50:54 -0500

    Added type definitions that are missing in Arduino IDE 1.0.

--- DirectIO/base.h
+++ DirectIO/base.h
@@ -19,12 +19,14 @@
 
 #include <Arduino.h>
 
+#if ARDUINO >= 150
 // for u8, u16, u32
 #include <USBAPI.h>
-
-// typedef uint8_t  u8;
-// typedef uint16_t u16;
-// typedef uint32_t u32;
+#else
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+#endif
 
 typedef int8_t  i8;
 typedef int16_t i16;
