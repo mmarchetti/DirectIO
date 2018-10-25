1f58edc9a5ed720a911a154e44aef1ff485d498a
Michael Marchetii <mike@themarchettis.com>
2018-10-25 14:49:28 +0000

    more 3rd party boards

---
+++ include/boards/samd/gemma_m0.h
@@ -0,0 +1,23 @@
+// DirectIO support for gemma_m0
+
+_define_pin(0, PORT_A, 4);
+_define_pin(1, PORT_A, 2);
+_define_pin(2, PORT_A, 5);
+_define_pin(3, PORT_A, 0);
+_define_pin(4, PORT_A, 1);
+_define_pin(5, PORT_A, 28);
+_define_pin(6, PORT_A, 24);
+_define_pin(7, PORT_A, 25);
+_define_pin(8, PORT_A, 2);
+_define_pin(9, PORT_A, 5);
+_define_pin(10, PORT_A, 4);
+_define_pin(11, PORT_A, 30);
+_define_pin(12, PORT_A, 31);
+_define_pin(13, PORT_A, 23);
+_define_pin(14, PORT_A, 10);
+_define_pin(15, PORT_A, 11);
+_define_pin(16, PORT_A, 6);
+_define_pin(17, PORT_A, 14);
+_define_pin(18, PORT_A, 15);
+_define_pin(19, PORT_A, 16);
+_define_pin(20, PORT_A, 17);
---
+++ include/boards/samd/pirkey.h
@@ -0,0 +1,33 @@
+// DirectIO support for pirkey
+
+_define_pin(0, PORT_A, 0);
+_define_pin(1, PORT_A, 1);
+_define_pin(2, PORT_A, 28);
+_define_pin(3, PORT_A, 30);
+_define_pin(4, PORT_A, 31);
+_define_pin(5, PORT_A, 14);
+_define_pin(6, PORT_A, 15);
+_define_pin(7, PORT_A, 27);
+_define_pin(8, PORT_A, 24);
+_define_pin(9, PORT_A, 25);
+_define_pin(10, PORT_A, 2);
+_define_pin(11, PORT_A, 9);
+_define_pin(12, PORT_A, 8);
+_define_pin(13, PORT_A, 7);
+_define_pin(14, PORT_A, 6);
+_define_pin(15, PORT_A, 6);
+_define_pin(16, PORT_A, 6);
+_define_pin(17, PORT_A, 6);
+_define_pin(18, PORT_A, 6);
+_define_pin(19, PORT_A, 6);
+_define_pin(20, PORT_A, 6);
+_define_pin(21, PORT_A, 6);
+_define_pin(22, PORT_A, 6);
+_define_pin(23, PORT_A, 6);
+_define_pin(24, PORT_A, 6);
+_define_pin(25, PORT_A, 6);
+_define_pin(26, PORT_A, 6);
+_define_pin(27, PORT_A, 6);
+_define_pin(28, PORT_A, 6);
+_define_pin(29, PORT_A, 6);
+_define_pin(30, PORT_A, 6);
