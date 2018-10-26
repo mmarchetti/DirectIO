b5119a39b65024fb40ded5d378fd703f19e940ef
Michael Marchetii <mike@themarchettis.com>
2018-10-26 15:27:58 +0000

    fix SAMD port structure

--- include/ports_samd.h
+++ include/ports_samd.h
@@ -17,7 +17,6 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-
 #ifndef _PORTS_SAMD_H
 #define _PORTS_SAMD_H 1
 
@@ -26,12 +25,13 @@ typedef volatile port_data_t* port_t;
 
 #define _define_port(NAME, PORTNUM) \
     struct NAME { \
-        static const u32 port = u32(&(REG_PORT_DIR##PORTNUM)); \
-        static inline u32 port_input_read() { return ((PortGroup*)port)->IN.reg; } \
-        static inline void port_output_write(u32 value) { ((PortGroup*)port)->OUT.reg = value; } \
-        static inline u32 port_output_read() { return ((PortGroup*)port)->OUT.reg; } \
-        static inline void port_enable_outputs(u32 mask) { ((PortGroup*)port)->DIRSET.reg = mask; } \
-        static inline void port_enable_inputs(u32 mask) { ((PortGroup*)port)->DIRCLR.reg = mask; } \
+        static inline u32 port_input_read() { return REG_PORT_IN##PORTNUM; } \
+        static inline void port_output_write(u32 value) { REG_PORT_OUT##PORTNUM = value; } \
+        static inline u32 port_output_read() { return REG_PORT_OUT##PORTNUM; } \
+        static inline void port_enable_outputs(u32 mask) { REG_PORT_DIRSET##PORTNUM = mask; } \
+        static inline void port_enable_inputs(u32 mask) { REG_PORT_DIRCLR##PORTNUM = mask; } \
+        static inline void port_output_set(u32 value) { REG_PORT_OUTSET##PORTNUM = value; } \
+        static inline void port_output_clear(u32 value) { REG_PORT_OUTCLR##PORTNUM = value; } \
     }
 
 #ifdef REG_PORT_DIR0
@@ -68,9 +68,9 @@ template <u8 pin> struct _pins {};
         static inline boolean input_read() { return (PORT::port_input_read() & mask) != 0; } \
         static inline boolean output_write(boolean value) { \
             if(value) { \
-               ((PortGroup*)port)->OUTSET.reg = mask; \
+                PORT::port_output_set(mask); \
             } else { \
-                 ((PortGroup*)port)->OUTCLR.reg = mask; \
+                PORT::port_output_clear(mask); \
             } \
         } \
         static inline boolean output_read() { return (PORT::port_output_read() & mask) != 0; } \
@@ -81,7 +81,37 @@ template <u8 pin> struct _pins {};
 #if defined(ARDUINO_SAM_ZERO)
 #include "boards/samd/arduino_zero.h"
 #elif defined(ARDUINO_SAMD_ZERO)
+// lots of boards define ARDUINO_SAMD_ZERO
+#if defined(ADAFRUIT_METRO_M0_EXPRESS)
+#include "boards/samd/metro_m0.h"
+#elif defined(ADAFRUIT_FEATHER_M0)
+#include "boards/samd/feather_m0.h"
+#elif USB_VID == 0x1B4F     // SparkFun Electronics
+#if USB_PID == 0x8D21
+// SparkFun SAMD21 Breakout doesn't define anything unique in build.extra_flags
+#if NUM_DIGITAL_PINS == 20
+#include "boards/samd/SparkFun_SAMD21_Dev.h"
+#elif NUM_DIGITAL_PINS == 14
+#include "boards/samd/SparkFun_SAMD_Mini.h"
+#else
+#warning "Unsupported Arduino SFE SAMD21 variant - falling back to digitalRead and digitalWrite."
+#define DIRECTIO_FALLBACK 1
+#endif  // NUM_DIGITAL_PINS
+#elif USB_PID == 0x0100
+#include "boards/samd/SparkFun_LilyMini.h"
+#elif USB_PID == 0x9D0E
+#include "boards/samd/SparkFun_9DoF_M0.h"
+#elif USB_PID == 0x214F
+#include "boards/samd/SparkFun_ProRF.h"
+#else
+// some other SFE USB_PID
+#warning "Unsupported Arduino SFE variant - falling back to digitalRead and digitalWrite."
+#define DIRECTIO_FALLBACK 1
+#endif  // USB_PID
+#else
+// not a special case board, just plain ARDUINO_SAMD_ZERO (aka arduino_mzero)
 #include "boards/samd/arduino_mzero.h"
+#endif  // ARDUINO_SAMD_ZERO
 #elif defined(ARDUINO_SAMD_MKR1000)
 #include "boards/samd/mkr1000.h"
 #elif defined(ARDUINO_SAMD_MKRZERO)
